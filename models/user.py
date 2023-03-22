5f62840d40845db10e8096f606cbf0d532ecd581
melekmoalla <melekmoalla2468@gmail.com>
2023-03-22 13:35:48 +0100

    test Update Place

--- models/user.py
+++ models/user.py
@@ -2,7 +2,7 @@
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
@@ -13,3 +13,5 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128))
     last_name = Column(String(128))
+    places = relationship('Place', backref='user',
+                          cascade='all, delete-orphan')
