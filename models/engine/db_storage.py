75a6c022c25e06f7a08c04cc9cb9c47cd5a53dca
melekmoalla <melekmoalla2468@gmail.com>
2023-03-22 11:34:09 +0100

    test

---
+++ models/engine/db_storage.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
+from os import environ
+from models.base_model import Base
+from models.user import User
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.city import City
+
+
+class DBStorage:
+    __engine = None
+    __session = None
+
+    def __init__(self):
+        user = 'HBNB_MYSQL_USER'
+        password = 'HBNB_MYSQL_PWD'
+        host = 'HBNB_MYSQL_HOST'
+        db = 'HBNB_MYSQL_DB'
+        self.__engine = create_engine(
+            'mysql+mysqldb://{user}:{password}@{host}/{db}',
+            pool_pre_ping=True)
+
+
+    def all(self, cls=None):
+        obj_dict = {}
+        if cls is None:
+            classes = [State, City, User, Amenity, Place, Review]
+        else:
+            classes = [cls]
+        for cls in classes:
+            objects = self.__session.query(cls).all()
+            for obj in objects:
+                key = f"{obj.__class__.__name__}.{obj.id}"
+                obj_dict[key] = obj
+
+        return obj_dict
+
+    def new(self, obj):
+        self.__session.add(obj)
+
+    def save(self):
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        if obj:
+            self.__session.delete(obj)
+
+    def reload(self):
+        Base.metadata.create_all(self.__engine)
+        session_factory = sessionmaker(
+            bind=self.__engine,
+            expire_on_commit=False
+        )
+        self.__session = scoped_session(session_factory)
