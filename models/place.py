b741f78242ef0b3cce2feb224cbedb4a7e2a98e0
melekmoalla <melekmoalla2468@gmail.com>
2023-03-23 14:24:11 +0100

    test

--- models/place.py
+++ models/place.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey, INTEGER, Float
+from sqlalchemy import Column, String, ForeignKey, INTEGER, Float, Table
+
 from models.review import Review
 from sqlalchemy.orm import relationship
 from os import getenv
+metadata = Base.metadata
+place_amenity_table = Table('place_amenity', metadata, Column('place_id', String(60), ForeignKey('places.id'), primary_key=True, nullable=False),
+                            Column('amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True, nullable=False))
 
 
 class Place(BaseModel, Base):
