7674b216c8daa55273a549397bf1d78ba7f0f62b
melekmoalla <melekmoalla2468@gmail.com>
2023-03-23 14:53:33 +0100

    fix pep8

--- models/place.py
+++ models/place.py
@@ -7,8 +7,14 @@ from models.review import Review
 from sqlalchemy.orm import relationship
 from os import getenv
 metadata = Base.metadata
-place_amenity_table = Table('place_amenity', metadata, Column('place_id', String(60), ForeignKey('places.id'), primary_key=True, nullable=False),
-                            Column('amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True, nullable=False))
+place_amenity_table = Table('place_amenity',
+                            metadata, Column('place_id', String(60),
+                                             ForeignKey(
+                                'places.id'),
+                                primary_key=True, nullable=False),
+                            Column('amenity_id', String(60),
+                                   ForeignKey('amenities.id'),
+                                   primary_key=True, nullable=False))
 
 
 class Place(BaseModel, Base):
@@ -46,5 +52,3 @@ class Place(BaseModel, Base):
                 if (i.amenity_ids == self.id):
                     lis.append(i)
             return (lis)
-        
-
