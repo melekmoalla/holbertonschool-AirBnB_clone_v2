63a8d303b8c4a86dd52f331083e354fcab524382
melekmoalla <melekmoalla2468@gmail.com>
2023-04-10 16:05:06 +0100

    fix pep8

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -6,7 +6,8 @@ Your web application must be listening on 0.0.0.0, port 5000
 Routes:
 /: display “Hello HBNB!”
 /hbnb: display “HBNB”
-/c/<text>: display “C ” followed by the value of the text variable (replace underscore _ symbols with a space )
+/c/<text>: display “C ” followed by the value of the text variable
+(replace underscore _ symbols with a space )
 
 """
 from flask import Flask
