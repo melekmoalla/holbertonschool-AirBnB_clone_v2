5a586e01592c74624628288f5d3b54c1bf0d9c9b
melekmoalla <melekmoalla2468@gmail.com>
2023-04-10 16:34:09 +0100

    test

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+Write a script that starts a Flask web application:
+
+Your web application must be listening on 0.0.0.0, port 5000
+Routes:
+/: display “Hello HBNB!”
+/hbnb: display “HBNB”
+/c/<text>: display “C ”, followed by the value of the text
+variable (replace underscore _ symbols with a space )
+/python/<text>: display “Python ”, followed by the value
+of the text variable (replace underscore _ symbols with a space )
+The default value of text is “is cool”
+
+"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_hbnb():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hello1():
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def hello(text):
+    a = text.replace("_", " ")
+    return f"C {a}"
+
+
+@app.route("/python/<text>")
+def hello2(text="is cool"):
+    a = text.replace("_", " ")
+    return f"C {a}"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
