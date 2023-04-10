6f25a7a7290dcdcabdd3b8d252794d72f132c3b7
melekmoalla <melekmoalla2468@gmail.com>
2023-04-10 15:54:16 +0100

    test

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Write a script that starts a Flask web application:
+
+Your web application must be listening on 0.0.0.0, port 5000
+Routes:
+/: display “Hello HBNB!”
+/hbnb: display “HBNB”
+/c/<text>: display “C ” followed by the value of the text variable (replace underscore _ symbols with a space )
+
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_hbnb():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hello():
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def hello(text):
+    return f"C {text}"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
