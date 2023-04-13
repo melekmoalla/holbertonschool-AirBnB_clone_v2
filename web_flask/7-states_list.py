9475f2b25a0cfcdfb9200f2d9c755c2f5f51d8fa
melekmoalla <melekmoalla2468@gmail.com>
2023-04-13 01:38:56 +0100

    test found count

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -21,9 +21,7 @@ def diplay():
     return render_template('7-states_list.html', states=states)
 
 
-@app.teardown_appcontext
-def teardown_appcontext(ex):
-    storage.close()
+
 
 
 if __name__ == "__main__":
